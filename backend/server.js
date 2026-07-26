52abbd5ea4a9950a88dd2f668c123a1782b196f2
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-07-26 22:45:02 +0530

    models

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 console.log("MONGO_URI:", JSON.stringify(process.env.MONGO_URI));
 connectDB();
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.json({
     success: true,
